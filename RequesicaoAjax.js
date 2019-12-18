c79f6f667505d69582d0bab7435a813f9419b1f5
danilojssantos <danilojssantos1@gmail.com>
2019-12-18 11:23:50 -0300

    exmplo de Requisicao AJAX

---
+++ RequesicaoAjax.js
@@ -0,0 +1,11 @@
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET','https://api.github.com/users/danilojssantos')
+xhr.send(null);
+
+xhr.onreadystatechange = function()
+{
+    if (xhr.readyState === 4) {
+        console.log(JSON.parse(xhr.responseText));
+    }
+}
